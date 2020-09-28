0220a95c54e15139d008c949ae00e2644bc2064d
daye109 <daye109@nate.com>
2020-09-28 21:26:46 +0900

    HouseDetailMoveServlet 주석처리 -다예

--- binbang/src/com/binbang/house/controller/HouseDetailMoveServlet.java
+++ binbang/src/com/binbang/house/controller/HouseDetailMoveServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.binbang.member.model.service.BinbangService;
+//import com.binbang.member.model.service.BinbangService;
 
 /**
  * Servlet implementation class HouseDetailMoveServlet
