02939c0f97a8c2ab092b6e1427d59a6b21ee8d50
jinyy <jinyy@LAPTOP-NEGGR9NP>
2020-09-28 21:17:28 +0900

    #1  member -jy-

--- binbang/src/com/binbang/common/JDBCTemplate.java
+++ binbang/src/com/binbang/common/JDBCTemplate.java
@@ -1,5 +1,26 @@
 package com.binbang.common;
 
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+import com.binbang.common.JDBCTemplate;
+
 public class JDBCTemplate {
 
+	public static Connection getConnection() {
+		Connection conn=null;
+		try {
+			Properties prop=new Properties();		
+			String path=JDBCTemplate.class.getResource("/driver/driver.properties").getPath();
+			prop.load(new FileReader(path));
+			Class.forName(prop.getProperty("driver"));
+			conn=DriverManager.getConnection(prop.getProperty("url"),
+					prop.getProperty("user"),prop.getProperty("pw"));
+			conn.setAutoCommit(false);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}return conn;
+	}
 }
