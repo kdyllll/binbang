e798bb35d1274b0e86e21fd4c0eb16dad5a9f75f
인하준 <@DESKTOP-TQB8MJ2>
2020-09-29 21:27:18 +0900

    필터 js변경 하우스 다오 변경
    하우스 디테일 jsp 수정중
    하우스 디테일css 수정중
    하우스디테일 무브서블릿 수정중
    하우스 디테일 스와이퍼 변경
    리뷰 css,js,jsp 수정중
    
    -하준-

--- binbang/web/js/house/filter.js
+++ binbang/web/js/house/filter.js
@@ -1,7 +1,14 @@
 /**
  * 
  */
-  var swiper = new Swiper('.swiper-container', {
+  var swiper = new Swiper('.second', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+ var swiper = new Swiper('.three', {
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
