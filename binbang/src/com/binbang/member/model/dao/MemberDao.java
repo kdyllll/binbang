4a3db977b827fcda79b1e5f086d71d24269d9ef6
jinyy <jinyy@LAPTOP-NEGGR9NP>
2020-09-29 13:12:49 +0900

    sql -jy-

---
+++ binbang/src/com/binbang/member/model/dao/MemberDao.java
@@ -0,0 +1,26 @@
+package com.binbang.member.model.dao;
+
+import java.util.Properties;
+//import static com.binbang.common.JDBCTemplate.close;
+import static com.binbang.common.JDBCTemplate.getConnection;
+
+
+public class MemberDao {
+
+	private Properties prop=new Properties();
+	
+	//회원조회
+	
+	//회원가입
+	
+	//중복확인
+	
+	//mypage 조회
+	
+	//mypage 수정
+	
+	//mypage 탈퇴
+	
+	//mypage 비밀번호수정
+	
+}
---
+++ binbang/src/com/binbang/member/model/service/MemberService.java
@@ -0,0 +1,26 @@
+package com.binbang.member.model.service;
+
+import com.binbang.member.model.dao.MemberDao;
+//import static com.binbang.common.JDBCTemplate.close;
+//import static com.binbang.common.JDBCTemplate.commit;
+import static com.binbang.common.JDBCTemplate.getConnection;
+//import static com.binbang.common.JDBCTemplate.rollback;
+
+public class MemberService {
+
+	private MemberDao dao=new MemberDao();
+
+	//회원조회
+	
+	//회원가입
+	
+	//중복확인
+	
+	//mypage 조회
+	
+	//mypage 수정
+	
+	//mypage 탈퇴
+	
+	//mypage 비밀번호수정
+}
