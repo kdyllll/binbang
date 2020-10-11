cac8331264d0bde78754fb9e4a5df2ea215ece91
daye109 <daye109@nate.com>
2020-10-11 17:01:51 +0900

    loginPopUp 작성중 -다예

--- binbang/src/com/binbang/common/controller/LoginPopUpServlet.java
+++ binbang/src/com/binbang/common/controller/LoginPopUpServlet.java
@@ -27,7 +27,7 @@ public class LoginPopUpServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/views/common/loginPopUp.jsp").forward(request, response);
 	}
 
 	/**
