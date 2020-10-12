1a6a8ea22dd124ae5d4368ea240e873651778f05
jinyy <jinyy@LAPTOP-NEGGR9NP>
2020-10-12 13:35:14 +0900

    # 관심숙소 servlet/dao/service 이동 및 생성

---
+++ binbang/src/com/binbang/member/model/dao/FavoriteDao.java
@@ -0,0 +1,45 @@
+package com.binbang.member.model.dao;
+
+import static com.binbang.common.JDBCTemplate.close;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import com.binbang.member.model.vo.Favorite;
+
+public class FavoriteDao {
+	
+	private Properties prop=new Properties();
+	
+	public FavoriteDao() {
+		try {
+			String path=FavoriteDao.class.getResource("/sql/member/favorite_sql.properties").getPath();
+			prop.load(new FileReader(path));
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//관심숙소 폴더 생성
+	public int createFolder(Connection conn,Favorite f) {
+		PreparedStatement pstmt=null;
+		int result=0;
+		
+		try {
+			pstmt=conn.prepareStatement(prop.getProperty("createFolder"));
+			
+			result=pstmt.executeUpdate();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally {
+			close(pstmt);			
+		}return result;		
+	}
+	
+	
+	
+}
