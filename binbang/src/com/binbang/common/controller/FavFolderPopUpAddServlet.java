49ec40742bdbdda3ffd95dd25fa5cde330f5beff
DabyK <ta0319@naver.com>
2020-10-12 15:45:40 +0900

    #관심숙소 등록 구현중 -다희-

---
+++ binbang/src/com/binbang/common/controller/FavFolderPopUpAddServlet.java
@@ -0,0 +1,42 @@
+package com.binbang.common.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FavFolderPopUpAddServlet
+ */
+@WebServlet("/favFolderPopUpAdd")
+public class FavFolderPopUpAddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FavFolderPopUpAddServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getParameter("folderName");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
