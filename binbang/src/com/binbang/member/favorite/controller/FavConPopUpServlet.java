9a5156390e3f5f03a6aaa8cb1eddcc77bbe503cf
DabyK <ta0319@naver.com>
2020-10-13 19:03:33 +0900

    #관심숙소 팝업 하는 중 -다희-

--- binbang/src/com/binbang/member/favorite/controller/FavConPopUpServlet.java
+++ binbang/src/com/binbang/member/favorite/controller/FavConPopUpServlet.java
@@ -1,4 +1,4 @@
-package com.binbang.common.controller;
+package com.binbang.member.favorite.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -8,24 +8,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import com.binbang.member.model.dao.FavoriteDao;
 import com.binbang.member.model.service.FavoriteService;
-import com.binbang.member.model.vo.Favorite;
-import com.binbang.member.model.vo.Member;
 
 /**
- * Servlet implementation class FavFolderPopUpAddServlet
+ * Servlet implementation class FavConPopUpAddServlet
  */
-@WebServlet("/favFolderPopUpAdd")
-public class FavFolderPopUpAddServlet extends HttpServlet {
+@WebServlet("/favorite/favConPopUp")
+public class FavConPopUpServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public FavFolderPopUpAddServlet() {
+    public FavConPopUpServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -35,35 +31,33 @@ public class FavFolderPopUpAddServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		Favorite f=new Favorite();
-		HttpSession session = request.getSession(false);
-		Member m = (Member) session.getAttribute("m");;
-		f.setMemberNo(m.getMemberNo());
-		f.setFolderName(request.getParameter("folderName"));
-		
+		System.out.println("서블릿실행");
 		String houseNo=request.getParameter("houseNo");
+		String[] addFolArr=request.getParameterValues("addFolerNo");
+		String[] delFolArr=request.getParameterValues("addFolerNo");
 		
-		int result=new FavoriteService().createFolder(f);
+		System.out.println(houseNo);
+		System.out.println(addFolArr);
+		System.out.println(delFolArr);
+		
+
+		int result=0;
 		String msg="";
 		if(result>0) {
-			msg="폴더가 추가되었습니다.";
+			msg="관심숙소가 반영되었습니다..";
 		}else {
-			msg="폴더 추가에 실패했습니다.";
+			msg="관심숙소 추가/삭제에 실패했습니다.";
 		}
 		response.setContentType("text/html; charset=euc-kr"); //한글이 인코딩
 		PrintWriter out = response.getWriter(); //선언		   
 	    String str="";
 	    str = "<script language='javascript'>";
 	    str += "alert('"+ msg + "');";  
-	    //str += " window.opener.location.reload();"; //부모창 새로고침 
-	    //str += " location.reload();"; // 새로고침 
-	    //location.replace('<%request.getContextPath()%>/favoritePopUp?houseNo=81'); 
-	    str+="location.replace('favoritePopUp?houseNo=";
-	    str+=houseNo;
-	    str+="');";
+	    str += " window.opener.location.reload();"; //부모창 새로고침 
+	    str += "self.close();";   // 창닫기
 	    str += "</script>";
 	    out.print(str);
-	} 
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
